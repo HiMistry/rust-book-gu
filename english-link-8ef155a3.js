4b9b19c03e17c1bee02e23c7eee7cb05509fb9df
Himanshubhai <himanshubhai@gmail.com>
2026-08-01 02:18:12 +0530

    Deploy: fix structural corruption in ch04-01, ch05-01, ch09-01, ch09-02; move original page link to bottom

---
+++ english-link-8ef155a3.js
@@ -0,0 +1,13 @@
+(function () {
+  var page = location.pathname.split('/').pop();
+  if (!page || page === 'index.html' || page === '') return;
+  var enUrl = 'https://doc.rust-lang.org/stable/book/' + page;
+  var el = document.createElement('p');
+  el.id = 'english-link';
+  el.innerHTML = 'Original page: <a href="' + enUrl + '" target="_blank">' + enUrl + '</a>';
+  el.style.cssText = 'font-size:0.85em;color:#888;margin:24px 0 0 0';
+  var main = document.querySelector('main');
+  if (main) {
+    main.parentNode.insertBefore(el, main.nextSibling);
+  }
+})();
